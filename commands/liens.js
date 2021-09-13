319e5d6eb7dfca578010ea4ddcc57a6bb0164f62
Kaze <42282286+TheReal-Kaze@users.noreply.github.com>
2021-09-13 18:00:48 +0100

    Update liens.js

--- commands/liens.js
+++ commands/liens.js
@@ -4,8 +4,8 @@ module.exports = {
     
        run: message => {
          message.channel.send(new Discord.MessageEmbed()
-            .setTitle('**Voici les liens les plus importants à avoir sur Squid rp:**')
-            .setDescription('[**•Clique __ici__**]() pour acceder à notre magnifique site web. \n [**•__Ici__**]() pour ouvrir notre google sheet/doc où tu trouveras pleins d\'informations. \n [**•__Ici__**]() tu trouveras notre MapBat afin d\'avoir toutes les informations sur les bâtiments du RolePlay ville.) \n •Et tu accéderas [**__Ici__**]() à notre page Top-Serveur où tu as la possibilité de voter pour notre serveur afin d\'avoir plus de visibilité ! ')
+            .setTitle('**Tu peux voter pour nous toutes les 2h:**')
+            .setDescription(' [**•__Clique Ici__**](https://docs.google.com/spreadsheets/d/1AQkxSR7PtgIfxtFct11arfPYfI9fjPVrLAuOIPReznY) pour ouvrir notre google sheet/doc où tu trouveras pleins d\'informations.  \n •Et  [**__Ici__**](https://top-serveurs.net/unturned/fr-squid-rp) à notre page Top-Serveur où tu as la possibilité de voter pour notre serveur afin d\'avoir plus de visibilité ! ')
             .setColor('RANDOM')
             .setFooter(message.author.username, message.author.avatarURL())
             .setTimestamp()
@@ -13,6 +13,6 @@ module.exports = {
  
  
         },
-     name: 'liens'
+     name: 'vote'
      
      }
