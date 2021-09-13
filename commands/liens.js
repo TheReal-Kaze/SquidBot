7696fd4628aede7b9f7c074800ad6349873b972b
Kaze <42282286+TheReal-Kaze@users.noreply.github.com>
2021-09-13 17:22:12 +0100

    Update liens.js

--- commands/liens.js
+++ commands/liens.js
@@ -4,8 +4,8 @@ module.exports = {
     
        run: message => {
          message.channel.send(new Discord.MessageEmbed()
-            .setTitle('**Voici les liens les plus importants à avoir sur Oméga City:**')
-            .setDescription('[**•Clique __ici__**](https://omegacity.fr/) pour acceder à notre magnifique site web. \n [**•__Ici__**](https://docs.google.com/spreadsheets/d/1To8Meqgiz_AnGtHTCu6ikXNnB-T_FlN1fM107AJ5dFg/edit?usp=sharing) pour ouvrir notre google sheet/doc où tu trouveras pleins d\'informations. \n [**•__Ici__**](https://map.omegacity.fr/) tu trouveras notre MapBat afin d\'avoir toutes les informations sur les bâtiments du RolePlay ville.) \n •Et tu accéderas [**__Ici__**](https://top-serveurs.net/unturned/fr-omega-city-rp) à notre page Top-Serveur où tu as la possibilité de voter pour notre serveur afin d\'avoir plus de visibilité ! ')
+            .setTitle('**Voici les liens les plus importants à avoir sur Squid rp:**')
+            .setDescription('[**•Clique __ici__**]() pour acceder à notre magnifique site web. \n [**•__Ici__**]() pour ouvrir notre google sheet/doc où tu trouveras pleins d\'informations. \n [**•__Ici__**]() tu trouveras notre MapBat afin d\'avoir toutes les informations sur les bâtiments du RolePlay ville.) \n •Et tu accéderas [**__Ici__**]() à notre page Top-Serveur où tu as la possibilité de voter pour notre serveur afin d\'avoir plus de visibilité ! ')
             .setColor('RANDOM')
             .setFooter(message.author.username, message.author.avatarURL())
             .setTimestamp()
