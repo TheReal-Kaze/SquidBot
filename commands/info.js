b2f357d24b3d610b0f4364f4cb724bdd0bd1bf6f
Kaze <42282286+TheReal-Kaze@users.noreply.github.com>
2021-09-13 17:20:58 +0100

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -7,20 +7,11 @@ module.exports = {
         
         .setDescription(`**__[FR] Oméga City [RP]; [MAINTENANCE]__**
 
-        IP du serveur: **__51.255.220.26__**
+        IP du serveur: **__6__**
         Port du serveur: **__27015__**
         
         
-        **__[FR] Oméga WW2 [RP]; [ON]__**
-
-        IP du serveur: **__178.32.107.69__**
-        Port du serveur: **__27015__**
-        
-        
-        **__[FR] Oméga Survie [Semi-RP]; [ON]__**
-
-        IP du serveur: **__135.125.113.65__**
-        Port du serveur: **__27015__** `)
+         `)
         
         .setColor('RANDOM'))
 
